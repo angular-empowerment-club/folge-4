b40e18c3637e1e1b95c32b14507fa6b1668edb28
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-01-18 00:23:43 +0100

    app: add enter animation for stock-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 import { StockQuote } from './models';
@@ -5,7 +12,19 @@ import { StockQuote } from './models';
 @Component({
   selector: 'aec-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateX(-100%)' }),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(100, style({ opacity: 1, transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   tsla: StockQuote = {
