88c5634bae224e52899aeec7f3abf7ca4c8f542f
Gregor Woiwode <gregor.woiwode@sap.com>
2018-02-01 08:52:17 +0100

    risk-switcher: enable user to select risk level of a stock quote

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,2 +1,3 @@
 export * from './stock-quote';
+export * from './stock-quote-risk';
 export * from './stock-quote-details';
---
+++ src/app/models/stock-quote-risk.ts
@@ -0,0 +1,5 @@
+export enum StockQuoteRisk {
+  Low,
+  Medium,
+  High
+}
